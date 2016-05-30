f63544f77c5440c5b1ccea43df372c03a690077c
Deniz Kusefoglu <denizk@gmail.com>
2016-05-30 14:31:26 +0300

    notices and messages working

--- js/script.js
+++ js/script.js
@@ -1,88 +1,80 @@
 
-var bgArray=[ 	"img/backgrounds/1.jpg",
-				"img/backgrounds/2.jpg",
-				"img/backgrounds/3.jpg",
-				"img/backgrounds/4.jpg"	];
-i=0;
-$(".full").css("background","url("+bgArray[i]+") no-repeat center center fixed");
-$(".full").css("background-size","cover");
+var bgArray=[ {url: "img/backgrounds/1.jpg", annote: "\'Mountain\' by Atölye member Deniz"},
+			{url: "img/backgrounds/2.jpg", annote: "\'Alleyway\' by Atölye member Deniz"},
+			{url: "img/backgrounds/3.jpg", annote: "\'Looking up!\' by Atölye member Deniz"},
+			{url: "img/backgrounds/4.jpg", annote: "\'Waves\' by Atölye member Deniz"}	];
 
-function addBG(newBG) {
+function addBG(newBG, user, timer) {
 	$(".full").css("background","url("+newBG+") no-repeat center center fixed");
 	$(".full").css("background-size","cover");
-	bgArray.push(newBG);
+	$(".bganno").text("image submitted by Atölye member "+user);
+	bgArray.push({url:newBG, annotate: "image submitted by Atölye member "+user});
+	console.log(bgArray);
+	clearInterval(timer);
+  	var timer= setInterval(swipeBG, 30000);
 }
 
 function swipeBG (){
-	i++;
-	i = i % bgArray.length;
-	$(".full").css("background","url("+bgArray[i]+") no-repeat center center fixed");
+	if (typeof(i)==="undefined"){var i=0;}
+	else {i = (i+1) % bgArray.length; }
+
+	$(".full").css("background","url("+bgArray[i].url+") no-repeat center center fixed");
 	$(".full").css("background-size","cover");
+	$(".bganno").text(bgArray[i].annote);
 }
+
+var timer= setInterval(swipeBG, 10000);
 
 var xhr = new XMLHttpRequest();
 xhr.open("GET", token, false);
 xhr.send();
-initresponse= JSON.parse(xhr.response); 
+initresponse= JSON.parse(xhr.response);
 url= initresponse.url;
-
 users= initresponse.users;
 var usersbynum={};
 users.forEach(function(elt){usersbynum[elt.id]=elt; });
-
 //https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_client_applications
 var sock = new WebSocket(url);
 sock.onmessage = function (event) {
 	thisevent= JSON.parse(event.data);
 	if (thisevent.type==="message"){
 		userid= thisevent.user;
-		var iurl= usersbynum[userid].profile.image_32;
-		activeFab();     // this should be open rather than toggle.
+		var username= usersbynum[userid].real_name;
+		console.log(usersbynum);
+		var iurl= usersbynum[userid].profile.image_512;
 		if (thisevent.text.slice(0,6)=== 'speak:'){
-			userSend(thisevent.text.slice(7),iurl);
-			botSpeak(thisevent.text.slice(7));
-		 }
-		 else if (thisevent.text.slice(0,3)=== 'bg:'){
-		 	addBG(thisevent.text.slice(4,-1));
-		 }
-		 else{ userSend(thisevent.text,iurl);}
-	}
-};
+			$(".messageim").attr("src", iurl);
+			$(".msg").fadeIn(300);
+			$(".msg p").text('\"'+thisevent.text.slice(7)+'\"');
+			setTimeout(function(){ $(".msg").fadeOut(300); }, 10000);
+			if ('speechSynthesis' in window) {
+				var msg = new SpeechSynthesisUtterance(thisevent.text.slice(7));
+			    window.speechSynthesis.speak(msg);
+			}
+		}
+		else if (thisevent.text.slice(0,3)=== 'bg:') {
+			addBG(thisevent.text.slice(4,-1),username, timer);
+		}
+		else {
+			$(".messageim").attr("src", iurl);
+			$(".msg").fadeIn(300);
+			$(".msg p").text('\"'+thisevent.text+'\"');
+			setTimeout(function(){ $(".msg").fadeOut(300); }, 10000);
+			// console.log($(".msg").display);
+		}
+}};
 
-setInterval(swipeBG, 3000);
-
-
-
-
-
-
-
-
-
-
-
-
-//////////////////////////////////////////////////////////// SLACBOTS.JS SOLUTION>>>>>
-// var SlackBot = require('slackbots');
-// // create a bot
-// var bot = new SlackBot({
-//     token: 'xoxb-32090694308-jaywKcTWKCtRb3AF4lipnhcd', // Add a bot https://my.slack.com/services/new/bot and put the token
-//     name: 'wallbot'
-// });
-// bot.on('start', function() {
-//     // more information about additional params https://api.slack.com/methods/chat.postMessage
-//     var params = {
-//         icon_emoji: ':cat:'
-//     };
-//     // // define channel, where bot exist. You can adjust it there https://my.slack.com/services
-//     bot.postMessageToChannel('wallbotchannel', 'meow!', params);
-//     // define existing username instead of 'user_name'
-//     bot.postMessageToUser('denizkusef', 'meow!', params);
-//     // // define private group instead of 'private_group', where bot exist
-//     // bot.postMessageToGroup('private_group', 'meow!', params);
-// });
-// alldata= bot.on('message', function(data) {
-//     // all ingoing events https://api.slack.com/rtm
-//     console.log(data);
-// });
-// botbot= alldata; // to prevent overwrite
+// $(".messageim").attr("src", 'img/arduino.jpg');
+// $(".msg").fadeIn(300);
+// var userSend= function(text, url){
+// 	$(".msg").fadeIn(300);
+// // 	setTimeout(3000);
+// // 	$(".pop").fadeOut(300);
+// };
+// var notificationtimer= setInterval(notify, 30000);
+// var notify= function(){
+// 	// .msg.display=== "block";
+// 	$(".notice").fadeIn(300);
+// 	setTimeout(function(){ $(".notice").fadeOut(300); }, 10000);
+// };
+	// $(".notice").fadeIn(300);
