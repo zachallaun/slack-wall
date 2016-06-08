378b0065261cef6edc143cc8233eb9438e9bc92c
Deniz Kusefoglu <denizk@gmail.com>
2016-06-08 21:20:04 +0300

    rounded user images and fixed username

--- js/script.js
+++ js/script.js
@@ -5,8 +5,6 @@ var bgArray=[
 			{url: "img/backgrounds/3.jpg", annote: "\'Looking up!\' by Atölye member Deniz"},
 			{url: "img/backgrounds/4.jpg", annote: "\'Waves\' by Atölye member Deniz"}	];
 
-// {url: "http://upload.wikimedia.org/wikipedia/commons/d/dd/Muybridge_race_horse_animated.gif", annote: " "},
-
 function swipeBG (i){
 	if (typeof(i)==="undefined"){var i=0;}
 	else { i = (i+1) % bgArray.length; }
@@ -45,7 +43,7 @@ function slackConnect(messageOnTime, timer){
 			var username= usersbynum[userid].real_name;
 			var iurl= usersbynum[userid].profile.image_512;
 
-			if (thisevent.subtype !== null && thisevent.subtype === "file_share"){       // Drag & Drop image
+			if (thisevent.subtype !== null && thisevent.subtype === "file_share"){					// Drag & Drop image
 // console.log(thisevent);
 				var pic_url= thisevent.file.url_private;
 				var pic_dl = thisevent.file.url_private_download;
@@ -58,30 +56,29 @@ function slackConnect(messageOnTime, timer){
 				setTimeout(function(){ $(".msg").fadeOut(300); }, messageOnTime);
 				if ('speechSynthesis' in window) {
 					var msg = new SpeechSynthesisUtterance(thisevent.text.slice(7));
-			    	window.speechSynthesis.speak(msg);
+					window.speechSynthesis.speak(msg);
 				}
 			} else if (thisevent.text.slice(0,3)=== 'bg:') {								// bg image url
 				timer= addBG(thisevent.text.slice(4,-1), username, timer);
 			} else {																		// Regular message
 				$(".messageim").attr("src", iurl);
 				$(".msg").fadeIn(300);
+				$(".name").text(username);
 				$(".msg p").text('\"'+thisevent.text+'\"');
-				setTimeout(function(){ $(".msg").fadeOut(300); }, messageOnTime);
+				setTimeout(function(){ $(".msg").fadeOut(600); }, messageOnTime);
 			}
 			return timer;
-		}}; }
+		}};}
 
-
-var bgInterval=5000;
-var noticeInterval=5000;
-var messageOnTime= 5000;
-var counter= -1;
+var bgInterval = 5000;
+var noticeInterval = 5000;
+var messageOnTime = 5000;
+var counter = -1;
 
 window.onload = function(){
 	var timer= setInterval(function(){counter = swipeBG(counter);}, bgInterval);
 	slackConnect(messageOnTime, timer);
-	// $(".messageim").attr("src", 'img/backgrounds/3.jpg');
+	// $(".messageim").attr("src", 'img/port.jpg');
 	// $(".msg").fadeIn(300);
-	// $(".msg p").text('\"hey\"');
-	// setTimeout(function(){ $(".msg").fadeOut(300); }, messageOnTime);
+	// $(".name").text('deniz');
 };
